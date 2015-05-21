5a4f76c684c43be0ef8b056a44be3e6083db5d56
Jeff Purcell <jeffrey.a.purcell@navy.mil>
2015-05-21 08:50:36 -0700

    Added DependencyResolverAdapter.

---
+++ src/Psns.Common.Web.Adapters/DependencyResolverAdapter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+
+namespace Psns.Common.Web.Adapters
+{
+    /// <summary>
+    /// Intended to be used only where Ninject constructor injection isn't possible (i.e. Model Binders)
+    /// </summary>
+    public class DependencyResolverAdapter
+    {
+        static IDependencyResolver _resolver;
+
+        /// <summary>
+        /// Unless set, System.Web.Mvc.DependencyResolver.Current is returned
+        /// </summary>
+        public static IDependencyResolver Resolver
+        {
+            get
+            {
+                return _resolver ?? DependencyResolver.Current;
+            }
+
+            set { _resolver = value; }
+        }
+    }
+}
